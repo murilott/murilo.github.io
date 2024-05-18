95e4ed48b3e2116fd8034d3a7a7643d7aff35d7a
murilo <112010004+murilott@users.noreply.github.com>
2024-05-17 21:57:19 -0300

    Update Main.java

--- POO II/Exerc1/Main.java
+++ POO II/Exerc1/Main.java
@@ -1,23 +1,6 @@
 
 public class Main {
   public static void main(String[] args) {
-    // Animal morcego = new Animal();
-    // morcego.setNome("Morci");
-    // // morcego.emitirSom();
-    // Gato gato = new Gato();
-    // gato.setNome("gatin");
-    // gato.emitirSom(TipodeSom.ASSUSTADO);
-    // // gato.emitirSom();
-    // Cachorro cach = new Cachorro();
-    // cach.setNome("Cadelin");
-    // // cach.emitirSom();
-    // // System.out.println("Hello world!");
-
-    // Zoo zoo = new Zoo();
-    // zoo.qualSomAnimal(morcego);
-    // zoo.qualSomAnimal(gato);
-    // zoo.qualSomAnimal(cach);
-
     // 2
     // Crie um programa para calcular o sal´ario dos funcion´arios de uma
     // empresa. O sal´ario ´e baseado no valor hora e quantidade de horas
